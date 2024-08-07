8bc120e274b9969f89dc033db290530292bcf0a5
Eric Aguayo <computergeek90@protonmail.com>
2024-08-07 15:40:45 -0500

    dark mode switcher for body push

--- social-media-dashboard-with-theme-switcher-master/js/dashboard.js
+++ social-media-dashboard-with-theme-switcher-master/js/dashboard.js
@@ -1,6 +1,7 @@
 const toggle = document.getElementById('toggle');
-const themeSwitcher = document.getElementById('id');
+const themeSwitcher = document.querySelectorAll('.followers-box');;
+
 
 toggle.addEventListener('change', function(){
-    
+    document.body.classList.toggle('dark-theme');
 })
