af54b14ceeaaa23c08290b6cac9625b47ab68335
Eric Aguayo <computergeek90@protonmail.com>
2024-08-07 16:20:52 -0500

    second theme toggle color switch test

--- social-media-dashboard-with-theme-switcher-master/js/dashboard.js
+++ social-media-dashboard-with-theme-switcher-master/js/dashboard.js
@@ -1,6 +1,4 @@
 const toggle = document.getElementById('toggle');
-const themeSwitcher = document.querySelectorAll('.followers-box');;
-
 
 toggle.addEventListener('change', function(){
     document.body.classList.toggle('dark-theme');
